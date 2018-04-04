43dad0636571ef92289791df93ace146e03dddc2
rishikagupta2437 <rishikagupta2013@gmail,com>
2018-04-04 20:26:19 +0530

    overriden methods with toast

--- app/src/main/java/com/example/rishika/helloworld/MainActivity.java
+++ app/src/main/java/com/example/rishika/helloworld/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.rishika.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"HELLO WORLD",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume() {
+
+        Toast.makeText(this,"RESUMED",Toast.LENGTH_LONG).show();
+        super.onResume();
+    }
+
+     @Override
+    protected void onPause() {
+
+        Toast.makeText(this,"PAUSE",Toast.LENGTH_LONG).show();
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+
+        Toast.makeText(this,"STOP",Toast.LENGTH_LONG).show();
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+
+
+        super.onDestroy();
     }
 }
